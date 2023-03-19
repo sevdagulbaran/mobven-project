154e7f8aadf4931048460b2b77c9141388301865
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2023-03-20 00:36:25 +0300

    Added UIView+Ext

--- MobvenProject/MobvenProject/Utils/Extensions/UIView+Ext.swift
+++ MobvenProject/MobvenProject/Utils/Extensions/UIView+Ext.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 extension UIView {
-
+    
+  public func roundCorners(_ corners:UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
 }
 
