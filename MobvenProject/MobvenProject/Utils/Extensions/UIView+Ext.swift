8e3cdaf0b068224702ac2de263c5be403218ae3a
sevdagulbaran <74216005+sevdagulbaran@users.noreply.github.com>
2023-03-20 02:46:28 +0300

    Uptaded roundCorner

--- MobvenProject/MobvenProject/Utils/Extensions/UIView+Ext.swift
+++ MobvenProject/MobvenProject/Utils/Extensions/UIView+Ext.swift
@@ -9,11 +9,18 @@ import UIKit
 
 extension UIView {
     
-  public func roundCorners(_ corners:UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        let mask = CAShapeLayer()
-        mask.path = path.cgPath
-        self.layer.mask = mask
+    // MARK: - Round Specific Corners
+    
+    func roundCorner(_ corners: UIRectCorner, radius: CGFloat) {
+        if #available(iOS 11.0, *) {
+            clipsToBounds = true
+            layer.cornerRadius = radius
+            layer.maskedCorners = CACornerMask(rawValue: corners.rawValue)
+        } else {
+            let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            layer.mask = mask
+        }
     }
 }
-
